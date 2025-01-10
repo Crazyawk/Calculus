02553bfac90ede27151e0ce69e41dcbbac5fadd2
Crazyawk <143974450+Crazyawk@users.noreply.github.com>
2025-01-09 21:35:37 -0500

    Update firebase-config.js
    i. need. help. now.

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAYKcZJjTF9aKLG1MsXXl5oSCdukSl1Qj8",
   authDomain: "science-49b9b.firebaseapp.com",
   projectId: "science-49b9b",
-  storageBucket: "science-49b9b.firebasestorage.app",
+  storageBucket: "science-49b9b.appspot.com", // Corrected storage bucket
   messagingSenderId: "924373786171",
   appId: "1:924373786171:web:66e157ef75a9e59cc8861e",
   measurementId: "G-ZS0X0K1F03"
